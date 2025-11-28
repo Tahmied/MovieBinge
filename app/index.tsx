856a6fdfc219234359b5d483866aed190255ce43
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-28 13:10:26 +0600

    - added initial basic routing and normal page setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Movie App.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-4xl text-primary font-bold">MovieBinge</Text>
+      <Link href={'/movies/ironman'}>
+        <Text className="text-4xl text-primary font-bold">Movie Details</Text>
+      </Link>
+
     </View>
   );
 }
---
+++ app/movies/[id].tsx
@@ -0,0 +1,16 @@
+import { useLocalSearchParams } from 'expo-router'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MovieDetails = () => {
+    const { id } = useLocalSearchParams()
+    return (
+        <View className='flex-1 justify-center items-center'>
+            <Text className='text-4xl color-secondary font-bold capitalize'>{id}</Text>
+        </View>
+    )
+}
+
+export default MovieDetails
+
+const styles = StyleSheet.create({})
