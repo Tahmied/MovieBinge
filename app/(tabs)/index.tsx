815a36bb117380895b46b873b8af90746d447422
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-30 16:14:41 +0600

    - added search bar in the home UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,23 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter()
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
 
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
-        <Image className="w-12 h-10 mt-20 mx-auto mb-5" source={icons.logo} />
-      </ScrollView>
 
+        <Image className="w-12 h-10 mt-20 mx-auto mb-5" source={icons.logo} />
+
+        <View className="flex-1 mt-5">
+          <SearchBar onPress={() => router.push('/search')} placeholder="Search for a movie" />
+        </View>
+      </ScrollView>
     </View>
   );
 }
